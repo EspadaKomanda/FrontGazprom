59cccf1cb097e4176af7c763455995ebc768e6ff
HBB <maxdo1579@mail.ru>
2024-06-22 23:51:03 +0300

    bibl page

---
+++ gazprom_front/src/app/biblioteca/page.js
@@ -0,0 +1,11 @@
+export default function Biblioteca() {
+
+    return (
+        <>
+            {
+                
+            }
+        </>
+    );
+
+}
---
+++ gazprom_front/src/components/search_bar.js
@@ -0,0 +1,9 @@
+export default function Search_bar({props}) {
+
+    return (
+        <>
+            <input type="text" className="search-bar w-1/3 h-10 px-2 py-1 rounded-lg text-gray-500" placeholder="Поиск..." />
+        </>
+    )
+
+}
