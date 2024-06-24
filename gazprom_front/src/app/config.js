c6e9b270dcc5853ca8fb91ba3b0d47f15011c7b3
HBB <maxdo1579@mail.ru>
2024-06-24 03:10:14 +0300

    fix requests to bibl

--- gazprom_front/src/app/config.js
+++ gazprom_front/src/app/config.js
@@ -1,8 +1,10 @@
 const config = {
-    Auth: 'http://176.57.213.45:5220/Auth/login',
-    User: 'http://api',
-    Message: 'http://api/Message/send-message',
-    ImageAgrigation: 'http://176.57.213.45:5220/ImageAgregation/generateImage',
+    getImagesPage: "http://90.156.218.15:5220/ImageAgregation/getImagesPage/",
+    getImagesZip: "http://90.156.218.15:5220/ImageAgregation/getSpecificImages",
+    getPagesCount: "http://90.156.218.15:5220/ImageAgregation/getPagesCount",
+    getImagesByKeyWords: "http://90.156.218.15:5220/ImageAgregation/getImagesByKeywords",
+    getRoles: "http://90.156.218.15:5220/Roles/getRoles",
+    getKeyWords: "http://90.156.218.15:5220/ImageAgregation/getUniqueKeyWords",
 }
 
 export default config;
